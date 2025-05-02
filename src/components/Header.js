a17e6d1c0cde0fa1f148876b7c994acce3c22beb
Anshul Rustogi <65132812+AnshulRustogi@users.noreply.github.com>
2025-05-03 03:08:22 +0530

    Add github action to publish website

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,28 +37,10 @@ const Header = ({ toggleTheme }) => {
                     >
                         {isDarkMode ? (
                             <span className="flex justify-center items-center h-full w-full">
-                                {/* White Moon and Star Icon */}
-                                {/* <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="currentColor"
-                                    viewBox="0 0 24 24"
-                                    className="w-5 h-5 text-gray-800 dark:text-white"
-                                >
-                                    <path d="M21.752 15.002a9.004 9.004 0 0 1-10.754-10.75 9.004 9.004 0 1 0 10.754 10.75z" />
-                                </svg> */}
                                 🌑
                             </span>
                         ) : (
                             <span className="flex justify-center items-center h-full w-full">
-                                {/* White Sun Icon */}
-                                {/* <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="currentColor"
-                                    viewBox="0 0 24 24"
-                                    className="w-5 h-5 text-gray-800 dark:text-white"
-                                >
-                                    <path d="M12 4a1 1 0 0 1 1-1h0a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h0a1 1 0 0 1-1-1V4zm0 14a1 1 0 0 1 1-1h0a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h0a1 1 0 0 1-1-1v-2zm8-6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h0a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zm-14 0a1 1 0 0 1 1-1H4a1 1 0 0 1-1 1h0a1 1 0 0 1 1 1h2a1 1 0 0 1-1-1zm11.071-7.071a1 1 0 0 1 1.414 0l1.414 1.414a1 1 0 0 1-1.414 1.414L16.07 4.343a1 1 0 0 1 0-1.414zm-8.485 0a1 1 0 0 1 1.414 0L8.414 5.757a1 1 0 0 1-1.414 1.414L5.586 5.757a1 1 0 0 1 0-1.414zm11.314 11.314a1 1 0 0 1 1.414 0l1.414 1.414a1 1 0 0 1-1.414 1.414l-1.414-1.414a1 1 0 0 1 0-1.414zm-11.314 0a1 1 0 0 1 1.414 0l1.414 1.414a1 1 0 0 1-1.414 1.414L4.343 16.07a1 1 0 0 1 0-1.414zM12 8a4 4 0 1 1 0 8 4 4 0 0 1 0-8z" />
-                                </svg> */}
                                 ☀
                             </span>
                         )}
